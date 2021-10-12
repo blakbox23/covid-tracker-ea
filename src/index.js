46b6f753341342c0f1350935db8da5abb39ae619
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-10-12 08:34:58 +0300

    set up redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    {/* <Provider store={store}> */}
     <App />
+    {/* </Provider> */}
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/redux/countries/countries.js
@@ -0,0 +1,23 @@
+// export const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+// export const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+// export const GET_BOOK = 'bookStore/books/GET_BOOK';
+
+// const initialState = [];
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_BOOK:
+//       return [...state, action.payload];
+
+//     case GET_BOOK:
+//       return action.formattedData;
+
+//     case REMOVE_BOOK:
+//       return state.filter((book) => book.item_id !== action.payload.item_id);
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export default reducer;
