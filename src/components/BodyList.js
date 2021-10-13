5bb029741088d792c7daa41b46b20094fb630a9d
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-10-13 23:19:22 +0300

    setup nav and header

--- src/components/BodyList.js
+++ src/components/BodyList.js
@@ -1,14 +1,15 @@
-/*eslint-disable*/
-import React, { useEffect } from 'react';
+/* eslint react/prop-types: 0 */import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getCountries } from '../redux/countries/countries';
 import { v4 as uuidv4 } from 'uuid';
+import { IconContext } from 'react-icons';
+import { GiAfrica } from 'react-icons/gi';
 import Nav from './Nav';
+import { getCountries } from '../redux/countries/countries';
 
 function BodyList() {
   const countries = useSelector((state) => state.data.countries);
- 
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,18 +18,30 @@ function BodyList() {
 
   return (
     <>
-     <Nav txt={'Home Navbar'}/>
+      <Nav txt="Home Navbar" />
+      <header className="header flex">
+        <div className="header-img">
+          <IconContext.Provider value={{ color: 'blue', className: 'africa-img' }}>
+            <div>
+              <GiAfrica />
+            </div>
+          </IconContext.Provider>
+        </div>
+        <div className="header-text">
+          <div>AFRICA</div>
+          <div>num</div>
+        </div>
+      </header>
       <h1>Africa Covid tracking</h1>
       <ul>
         {countries.map((country) => (
-        <li key={uuidv4()}>
+          <li key={uuidv4()}>
             <Link to={`/${country.country}`}>
               {country.country}
             </Link>
-            <div>{country.cases}</div>         
-            </li>
-        )
-        )}
+            <div>{country.cases}</div>
+          </li>
+        ))}
       </ul>
 
       {/* <ul>
